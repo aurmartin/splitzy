982f1ea287e4b3d1226b2d912ddbb45ea44e4237
aurmartin <aurmartin@pm.me>
2025-03-07 19:48:49 +0100

    build: add test files to metro blockList

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,6 @@ const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 
 const config = getSentryExpoConfig(__dirname);
 
+config.resolver.blockList = [/(\/__tests__\/.*)$/, /(\.test\.tsx?)$/];
+
 module.exports = config;
