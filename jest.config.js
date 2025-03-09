dfba28f27b584fc8d7e874746d31cfc50ab5683c
aurmartin <aurmartin@pm.me>
2025-03-09 18:32:16 +0100

    build: add codecov to ci

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@ module.exports = {
   transformIgnorePatterns: [
     "/node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|uuid|y-expo-sqlite|react-native-reanimated|)",
   ],
+  coverageReporters: ["lcov", "cobertura", "text"],
   collectCoverageFrom: [
     "app/**/*.{js,jsx,ts,tsx}",
     "components/**/*.{js,jsx,ts,tsx}",
